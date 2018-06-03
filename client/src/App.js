79b10f3a747adb27fa8a46d4cbb75305d1f45966
jesscia <jessicalunaone@gmail.com>
2018-06-03 10:24:49 +0000

    nav landing footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Profile</h1>
+      <div className="App">
+        <Navbar />
+        <Landing />
+        <Footer />
       </div>
     );
   }
